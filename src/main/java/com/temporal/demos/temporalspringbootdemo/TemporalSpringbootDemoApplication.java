f9c5b88a3d18cef84a8997c01f4fec22923dd942
Tihomir Surdilovic <tihomir@temporal.io>
2023-02-17 16:37:52 -0500

    update to 1.18.1
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>

--- src/main/java/com/temporal/demos/temporalspringbootdemo/TemporalSpringbootDemoApplication.java
+++ src/main/java/com/temporal/demos/temporalspringbootdemo/TemporalSpringbootDemoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TemporalSpringbootDemoApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(TemporalSpringbootDemoApplication.class, args);
+		SpringApplication.run(TemporalSpringbootDemoApplication.class, args).start();
 	}
 }
